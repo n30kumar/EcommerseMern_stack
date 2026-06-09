dbf2aa4228428256c5f60958c39d1183382b62dd
Your Name <your.email@example.com>
2026-06-09 16:28:58 +0530

    new functionality added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,12 +3,18 @@ import { register } from '../controllers/userController.js';
 import { verify } from '../controllers/userController.js';
 import { reVerify } from '../controllers/userController.js';
 import { login } from '../controllers/userController.js';
+import { logout } from '../controllers/userController.js';
+import { isAuthenticated } from '../middleware/isAuthenticated.js';
+import {forgotPassword} from '../controllers/userController.js';
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/verify', verify);
-router.post('/reVerify', verify);
+router.post('/reVerify', reVerify);
 router.post('/login',login);
+router.post('/logout',isAuthenticated,logout);
+router.post('/forget-password',isAuthenticated,forgotPassword);
+
 export default router;
 
 
